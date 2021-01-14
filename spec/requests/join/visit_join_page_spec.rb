4e9632d4a856faa6d92f0ab5647782c129c1750b
Matt Dominiak <mateusz.dominiak@gmail.com>
2021-01-14 09:00:28 +0100

    Add visit_join_page_spec

---
+++ spec/requests/join/visit_join_page_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "visit join page", type: :request do
+  subject { get join_path }
+
+  it 'responds with success' do
+    subject
+    
+    expect(response).to have_http_status(200)
+  end
+end
