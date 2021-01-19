8171a7f3cb5e252ea25f7bd9ae3dc47912d7d7e8
Matt Dominiak <mateusz.dominiak@gmail.com>
2021-01-19 19:11:13 +0100

    Style message editor

--- app/javascript/controllers/scroll_into_view_controller.js
+++ app/javascript/controllers/scroll_into_view_controller.js
@@ -2,11 +2,6 @@ import { Controller } from 'stimulus'
 
 export default class extends Controller {
   connect() {
-    if (document.querySelectorAll(`#${this.element.id}`).length > 1) {
-      this.element.remove()
-      return
-    }
-
     this.element.scrollIntoView({ block: 'nearest' })
   }
 }
