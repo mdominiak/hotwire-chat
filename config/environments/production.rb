b190427a0b74dcbf90e8738663b686b5def8b5b2
Matt Dominiak <mateusz.dominiak@gmail.com>
2021-01-20 18:22:02 +0100

    Cache _message partial

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,9 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  if ENV['REDIS_URL']
+    config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
+  end
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
